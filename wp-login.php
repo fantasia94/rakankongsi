7d32b388fc9c265186688e9c70737f6739cc0ae1
fantasia94 <34854102+fantasia94@users.noreply.github.com>
2018-12-26 17:47:21 +0800

    Add files via upload

---
+++ wp-login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Rakan Kongsi &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='https://fantasia94.github.io/rakankongsi/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='https://fantasia94.github.io/rakankongsi/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='https://fantasia94.github.io/rakankongsi/wp-content/plugins/ar-contactus/res/js/scripts.js?ver=1.3.2'></script>
+<script type='text/javascript' src='https://fantasia94.github.io/rakankongsi/wp-content/plugins/ar-contactus/res/js/jquery.contactus.min.js?ver=1.3.2'></script>
+<link rel='stylesheet' id='jquery.contactus.css-css'  href='https://fantasia94.github.io/rakankongsi/wp-content/plugins/ar-contactus/res/css/jquery.contactus.min.css?ver=1.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://fantasia94.github.io/rakankongsi/wp-includes/css/dashicons.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://fantasia94.github.io/rakankongsi/wp-includes/css/buttons.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://fantasia94.github.io/rakankongsi/wp-admin/css/forms.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://fantasia94.github.io/rakankongsi/wp-admin/css/l10n.min.css?ver=5.0.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://fantasia94.github.io/rakankongsi/wp-admin/css/login.min.css?ver=5.0.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="https://fantasia94.github.io/rakankongsi/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://fantasia94.github.io/rakankongsi/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://fantasia94.github.io/rakankongsi/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://fantasia94.github.io/rakankongsi/">&larr; Back to Rakan Kongsi</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
